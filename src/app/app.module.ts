0513013d9dd1bad76369a0a8f2c3df519407c091
Your Name <you@example.com>
2018-01-31 08:41:52 +0000

    add CreateComponent class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyModule } from './my.module';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
