2de742906a695d9e1ef80451a58afe364b6db9ff
Your Name <you@example.com>
2018-01-31 08:43:39 +0000

    add MoviesComponent class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyModule } from './my.module';
 import { CreateComponent } from './create/create.component';
+import { MoviesComponent } from './movies/movies.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateComponent
+    CreateComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
